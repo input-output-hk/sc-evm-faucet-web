a36ce79d32dc9565d58d3d8032cad38f22e635cd
Shay Bergmann <shay.bergmann@iohk.io>
2020-12-02 15:09:58 -0500

    Factor package out of flake file

--- flake.nix
+++ flake.nix
@@ -8,23 +8,7 @@
       name = "mantis-faucet-web";
       systems = [ "x86_64-linux" "x86_64-darwin" ];
       overlay = final: prev: {
-        ${name}.defaultPackage = final.mkYarnPackage {
-          src = ./.;
-
-          FAUCET_NODE_URL = "https://mantis-testnet-mantis-faucet.mantis.ws";
-
-          doCheck = false;
-          distPhase = "true";
-
-          buildPhase = ''
-            export HOME="$NIX_BUILD_TOP"
-            yarn run build
-          '';
-
-          installPhase = ''
-            mv deps/$pname/dist $out
-          '';
-        };
+        ${name}.defaultPackage = final.callPackage ./package.nix {};
       };
 
       simpleFlake = flake-utils.lib.simpleFlake {
---
+++ package.nix
@@ -0,0 +1,19 @@
+{ mkYarnPackage }:
+
+mkYarnPackage {
+  src = ./.;
+
+  FAUCET_NODE_URL = "https://mantis-testnet-mantis-faucet.mantis.ws";
+
+  doCheck = false;
+  distPhase = "true";
+
+  buildPhase = ''
+    export HOME="$NIX_BUILD_TOP"
+    yarn run build
+  '';
+
+  installPhase = ''
+    mv deps/$pname/dist $out
+  '';
+}
